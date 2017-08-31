d3dcb32fb76a9a086891ecc33cd46efc9dffe64f
SR3u <wnmsr3u@gmail.com>
2017-08-31 12:50:40 +0300

    Some fixes

--- src/main/java/org/civildefence/letovbot/message_handlers/StickerMessageHandler.java
+++ src/main/java/org/civildefence/letovbot/message_handlers/StickerMessageHandler.java
@@ -2,11 +2,13 @@ package org.civildefence.letovbot.message_handlers;
 
 import lombok.SneakyThrows;
 import org.civildefence.letovbot.LetovBot;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.stickers.Sticker;
 
 import java.io.InputStream;
+import java.net.URL;
 
 public class StickerMessageHandler implements MessageHandler {
     @Override
@@ -17,11 +19,17 @@ public class StickerMessageHandler implements MessageHandler {
                 @Override
                 @SneakyThrows
                 public void run() {
-                    InputStream stickerStream = bot.getFileInputStream(sticker.getFileId());
+                    String url = bot.getFileURL(bot.getFile(sticker.getFileId()));
+                    SendMessage sendMessage = new SendMessage()
+                            .setReplyToMessageId(message.getMessageId())
+                            .setChatId(message.getChatId())
+                            .setText(url);
+                    InputStream stickerStream = new URL(url).openStream();
                     SendPhoto sendPhoto = new SendPhoto()
                             .setNewPhoto(sticker.getEmoji(), stickerStream)
                             .setReplyToMessageId(message.getMessageId())
                             .setChatId(message.getChatId());
+                    //bot.sendMessage(sendMessage);
                     bot.sendPhoto(sendPhoto);
                     bot.withStateStorage(storage -> {
                         storage.withValue(message.getChat(), message.getFrom(), "StickerMessageHandler", sticker.getSetName(), 0L,
